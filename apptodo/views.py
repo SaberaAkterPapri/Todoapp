07faf21a8a8b3bd970b034df3a9ef511c89fefd8
Sabera Akter Papri <saberaakterpapri08@gmail.com>
2021-01-30 21:28:05 +0600

    To do App

---
+++ apptodo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import TodoItem
+
+# Create your views here.
+def todo(request):
+    all_todo_items = TodoItem.objects.all()
+    return render(request, 'todo.html',
+        {'all_items': all_todo_items})
+
+def addTodo(request):
+    # print(request.POST['content'])
+    if request.method =='POST':
+        new_item = TodoItem(content = request.POST['content'])
+        new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+    
+def deleteTodo(request, todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    # new_item = TodoItem(content = request.POST['content'])
+    # new_item.save()
+    return HttpResponseRedirect('/todo/')
